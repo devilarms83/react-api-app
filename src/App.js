ef355d3e09d3e9222b52fae4355f3b809752140d
devilarms83 <bautista.albert@gmail.com>
2022-10-25 19:27:01 -0500

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
+// import axios from 'axios'
 
 export default function App() {
+  /*
+  create state variables for users, setUsers
+  create a function that fetches the data on click
+  */
+  const [users, setUsers] = useState([])
+
+  const fetchUsers = async () => {
+    const apiUrl = 'https://randomuser.me/api/?results=10'
+    
+    try {
+      const response = await fetch(apiUrl)
+      const data = await response.json()
+      console.log(data)
+      setUsers(data.results)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+  console.table(users)
+
   return (
     <div>
       <h2>Fetch User Data</h2>
-      <button onClick={() => console.log('hi')}>
+      <button onClick={() => fetchUsers()}>
         Fetch Data
       </button>
       <button>Load Data</button>
